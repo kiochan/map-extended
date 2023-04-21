49a4c942afcbf47c896cf54bb1845252fb73d4fa
kiochan <sunxingchen@live.com>
2023-04-21 20:14:43 +0200

    basic test

---
+++ src/MapExt.test.ts
@@ -0,0 +1,11 @@
+import './MapExt'
+
+describe('MapExt', () => {
+  test('should be able to create instance', () => {
+    expect(new Map()).toBeInstanceOf(Map)
+  })
+
+  test('should be able to set a item', () => {
+    expect(new Map()).toBeInstanceOf(Map)
+  })
+})
